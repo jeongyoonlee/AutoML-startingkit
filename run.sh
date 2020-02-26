f6b241443fb6eeb75280fc967c6bf2fac1092b84
Jeong-Yoon Lee <jeong@uber.com>
2020-02-25 18:26:14 -0800

    update run.sh to exit when an error occurs

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Exit when any command fails.
+set -e
+
+# It requires two input arguments: the model and data folder names
 if [ "$1" = "" ] || [ "$2" = "" ]; then
     echo "USAGE: ./train.sh [model folder] [data folder]"
     exit 1
@@ -15,18 +19,22 @@ echo "Model directory: $1"
 echo "Data directory: $data_dir"
 echo "Output directory: $output_dir"
 
+# If the data folder doesn't exist, exit.
 if [ ! -d $data_dir ]; then
     echo "Data directory, $data_dir does not exist."
     exit 1
 fi
 
+# If the output folder doesn't exist, create it.
 if [ ! -d $output_dir ]; then
     echo "Output directory, $output_dir does not exist. Creating $output_dir..."
     mkdir -p $output_dir;
 fi
 
+# Train the auto ML model.
 echo "training..."
 python3 AutoML3_ingestion_program/ingestion.py $data_dir $output_dir $data_dir AutoML3_ingestion_program $model_dir
 
+# Predict for test data and score predictions.
 echo "predicting..."
 python3 AutoML3_scoring_program/score.py "$data_dir/*/" $output_dir $output_dir
